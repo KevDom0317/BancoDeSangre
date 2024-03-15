4f8975c876963b7f63345f43fd0fb3355a5fd093
Ariel <josearielmll545@gmail.com>
2024-03-15 07:02:47 -1100

    AgregarMenu

--- ModificarDonante.cs
+++ ModificarDonante.cs
@@ -1,6 +1,39 @@
+using System.Diagnostics;
+
 namespace BancoDeSangre;
 
 class ModificarDonante
 {
     public ModificarDonante(){}
+
+    public void MenuModificacion()
+    {
+        Console.WriteLine("Que tipo de modificación deséa realizar? \n1. Modificar tipo de sangre. \n2.Modificar RH.");
+        int opcion = int.Parse(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                ModificarTipo();
+                break;
+
+            case 2:
+                ModificarRH();
+                break;
+
+            default:
+                Console.WriteLine("Porfavor, selccione una opción permitida");
+                break;
+        }
+    }
+
+    public void ModificarTipo()
+    {
+
+    }
+
+    public void ModificarRH()
+    {
+
+    }
 }
